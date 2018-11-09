4afd827ae3c2843493e98d13a355409298fcf03b
Fabien Olousouzian <fabien@awayup.io>
2018-11-09 12:11:43 +0100

    Initial commit

---
+++ StackExchange.Redis.DataTypesCore/IRedisTypeFactory.cs
@@ -0,0 +1,11 @@
+﻿using StackExchange.Redis.DataTypesCore.Collections;
+
+namespace StackExchange.Redis.DataTypesCore
+{
+    public interface IRedisTypeFactory
+    {
+        RedisDictionary<TKey, TValue> GetDictionary<TKey, TValue>(string name);
+        RedisSet<T> GetSet<T>(string name);
+        RedisList<T> GetList<T>(string name);
+    }
+}
